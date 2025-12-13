9a666783f5c5b3af8b74d84b4328aa727b1e86fd
DanilSJ <danilchagarnoy@mail.ru>
2025-12-14 00:32:15 +0300

    Add new model user

---
+++ core/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Integer, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from .base import Base
+
+
+class User(Base):
+    name: Mapped[str] = mapped_column(String)
+    password: Mapped[str] = mapped_column(String)
+
+    phone: Mapped[int] = mapped_column(Integer)
+
+    room: Mapped[int] = mapped_column(Integer)
+
+    start_life: Mapped[DateTime] = mapped_column(DateTime)
+    end_life: Mapped[DateTime] = mapped_column(DateTime)
