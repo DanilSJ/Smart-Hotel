0bd910fdd20793ee45076b0882926669ef0c6a1d
DanilSJ <danilchagarnoy@mail.ru>
2025-12-14 21:59:21 +0300

    new func create_user

--- api_v1/user/crud.py
+++ api_v1/user/crud.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from sqlalchemy.engine import Result
+from sqlalchemy.exc import StatementError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from core.models import User
@@ -31,3 +32,22 @@ async def create_user(session: AsyncSession, user_in: schemas.RegisterSchema) ->
     session.add(user)
     await session.commit()
     return user
+
+async def update_user(session: AsyncSession, user_id: int, user_in: schemas.UpdateSchema) -> schemas.UserSchema:
+    stmt = select(User).where(User.id == user_id)
+    result = await session.execute(stmt)
+    user = result.scalars().first()
+
+    if user:
+        try:
+            for field, value in user_in.model_dump(exclude_unset=True).items():
+                setattr(user, field, value)
+
+            await session.commit()
+            await session.refresh(user)
+
+            return schemas.UserSchema.model_validate(user)
+        except StatementError:
+            raise HTTPException(status_code=400, detail="Incorrect arguments")
+
+    raise HTTPException(status_code=404, detail="User does not exist")
