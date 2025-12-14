a1b18733575c98f4aada5a3faa2e8bcad4e2ab60
DanilSJ <danilchagarnoy@mail.ru>
2025-12-14 21:30:30 +0300

    set echo on false

--- core/config.py
+++ core/config.py
@@ -6,6 +6,6 @@ BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
-    db_echo: bool = True
+    db_echo: bool = False
 
 settings = Settings()
