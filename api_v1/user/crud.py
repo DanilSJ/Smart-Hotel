ec3493a60ca0c3a6d8f79ff5535bd20b1336b085
DanilSJ <danilchagarnoy@mail.ru>
2025-12-14 21:38:52 +0300

    add hashed password and condition

--- api_v1/user/crud.py
+++ api_v1/user/crud.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
@@ -17,6 +18,15 @@ async def get_user(session: AsyncSession, user_id) -> User | None:
     return await session.get(User, user_id)
 
 async def create_user(session: AsyncSession, user_in: schemas.RegisterSchema) -> User:
+    stmt = select(User).where(User.name == user_in.name)
+    result: Result = await session.execute(stmt)
+
+    if result.scalars().first():
+        raise HTTPException(status_code=409, detail="User already exists")
+
+    hashed = bcrypt.hashpw(user_in.password, bcrypt.gensalt())
+    user_in.password = hashed
+
     user = User(**user_in.model_dump())
     session.add(user)
     await session.commit()
