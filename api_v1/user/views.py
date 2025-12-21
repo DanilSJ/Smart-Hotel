7d95a864effbf87a0dbdc734dd1f59b9b71bb51a
DanilSJ <danilchagarnoy@mail.ru>
2025-12-21 22:57:39 +0300

    add func login jwt

--- api_v1/user/views.py
+++ api_v1/user/views.py
@@ -1,9 +1,19 @@
-from fastapi import APIRouter, Depends, HTTPException, Response
+from datetime import timedelta
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
+
 from core.models import db_helper
 from . import schemas
 from . import crud
-
+from api_v1.auth.views import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    create_access_token,
+    get_current_user,
+    get_token,
+    Token,
+)
 
 router = APIRouter()
 
@@ -33,8 +43,25 @@ async def login_user(
     data: schemas.LoginSchema,
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
-    result = await crud.login_user(session, data.name, data.password)
-    return result
+    user = await crud.login_user(session, data.name, data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": str(user.id)}, expires_delta=access_token_expires
+    )
+
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.get("/auth/check/")
+async def auth_check(token: str = Depends(get_token)):
+    return await get_current_user(token)
 
 
 #
