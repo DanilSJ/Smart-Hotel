734c4359a53ed1e3debaaff2a8da0092d3f9abb2
DanilSJ <danilchagarnoy@mail.ru>
2025-12-22 23:21:09 +0300

    add AuthSettings

--- core/config.py
+++ core/config.py
@@ -1,11 +1,22 @@
+import os
 from pydantic_settings import BaseSettings
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
 
+class AuthSettings(BaseSettings):
+    ALGORITHM: str = "HS256"
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "KEY")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+
 class Settings(BaseSettings):
     db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     db_echo: bool = False
 
+    AuthSettings()
+
+
 settings = Settings()
+auth_settings = AuthSettings()
