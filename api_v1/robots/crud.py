2c349ab6f9022149bbc96a92d3d61cfdf1d817a5
DanilSJ <danilchagarnoy@mail.ru>
2025-12-31 20:52:43 +0300

    add get_delivery get_deliveries update_robot_deliver update_robot_status

--- api_v1/robots/crud.py
+++ api_v1/robots/crud.py
@@ -1,4 +1,4 @@
-from sqlalchemy.engine import Result
+from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from core.models import Robot
@@ -7,10 +7,64 @@ from . import schemas
 
 async def get_all_robots(session: AsyncSession) -> list[Robot]:
     stmt = select(Robot).order_by(Robot.id)
-    result: Result = await session.execute(stmt)
+    result = await session.execute(stmt)
     robots = result.scalars().all()
     return list(robots)
 
 
 async def get_robot(session: AsyncSession, robot_id) -> Robot | None:
+    return await session.get(Robot, robot_id)
+
+
+async def update_robot_status(session: AsyncSession, robot_id: int) -> Robot:
+    stmt = select(Robot).where(Robot.id == robot_id)
+    result = await session.execute(stmt)
+    robot = result.scalars().first()
+
+    if not robot:
+        raise HTTPException(status_code=404, detail="Robot not found")
+
+    if robot.status == "delivery":
+        robot.status = "delivery_completed"
+
+    if robot.status == "delivery_completed":
+        robot.status = "sleep"
+
+    if robot.status == "sleep":
+        robot.status = "delivery"
+
+    await session.commit()
+    await session.refresh(robot)
+
+    return robot
+
+
+async def update_robot_deliver(
+    session: AsyncSession, robot_id: int, room_id: int
+) -> Robot:
+    stmt = select(Robot).where(Robot.id == robot_id)
+    result = await session.execute(stmt)
+    robot = result.scalars().first()
+
+    if not robot:
+        raise HTTPException(status_code=404, detail="Robot not found")
+
+    robot.deliver = True
+    robot.deliver_status = "delivery"
+    robot.deliver_room = room_id
+
+    await session.commit()
+    await session.refresh(robot)
+
+    return robot
+
+
+async def get_deliveries(session: AsyncSession) -> list[Robot]:
+    stmt = select(Robot).filter(Robot.deliver == True)
+    result = await session.execute(stmt)
+    robots = result.scalars().all()
+    return list(robots)
+
+
+async def get_delivery(session: AsyncSession, robot_id) -> Robot | None:
     return await session.get(Robot, robot_id)
--- api_v1/robots/views.py
+++ api_v1/robots/views.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import db_helper
 from . import schemas
 from . import crud
+from api_v1.auth.views import get_token, get_current_user
+from api_v1.utils import check_administrator
 
 router = APIRouter()
 
@@ -14,9 +16,61 @@ async def get_all_robots(
     return await crud.get_all_robots(session)
 
 
-@router.get("/{id}/", response_model=schemas.RobotSchema)
+@router.get("/{robot_id}/", response_model=schemas.RobotSchema)
 async def get_robot(
     robot_id: int,
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.get_robot(session, robot_id)
+
+
+@router.put("/{robot_id}/status", response_model=schemas.RobotSchema)
+async def update_robot_status(
+    robot_id: int,
+    token: str = Depends(get_token),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    user_id = await get_current_user(token)
+    if not user_id:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+        )
+
+    await check_administrator(session, user_id)
+
+    return await crud.update_robot_status(session, robot_id)
+
+
+@router.put("/{robot_id}/deliver", response_model=schemas.RobotSchema)
+async def update_robot_deliver(
+    robot_id: int,
+    room_id: int,
+    token: str = Depends(get_token),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    user_id = await get_current_user(token)
+    if not user_id:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+        )
+
+    await check_administrator(session, user_id)
+
+    return await crud.update_robot_deliver(session, robot_id, room_id)
+
+
+@router.get("/deliveries/", response_model=list[schemas.RobotSchema])
+async def get_deliveries(
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.get_deliveries(session)
+
+
+@router.get("/deliveries/{robot_id}/", response_model=schemas.RobotSchema)
+async def get_delivery(
+    robot_id: int,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.get_delivery(session, robot_id)
