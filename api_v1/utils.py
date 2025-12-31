c1d351186e704e6dc0ead0eb094b647645474527
DanilSJ <danilchagarnoy@mail.ru>
2025-12-31 20:24:51 +0300

    add func check_administrator

--- api_v1/utils.py
+++ api_v1/utils.py
@@ -7,8 +7,6 @@ async def check_administrator(session, user_id) -> User:
     user = await get_user(session, user_id)
 
     if not user.admin:
-        raise HTTPException(
-            status_code=401, detail="You are not an admin"
-        )
+        raise HTTPException(status_code=401, detail="You are not an admin")
 
     return user
